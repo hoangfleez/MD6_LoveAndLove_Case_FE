3ad0519a4a2d45ea1617035b36644ede29080582
hoangfsama <leehoagf@gmail.com>
2023-06-13 01:25:23 +0700

    fix validate

--- src/page/user/Register.jsx
+++ src/page/user/Register.jsx
@@ -1,19 +1,17 @@
 import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import VolunteerActivismIcon from "@mui/icons-material/VolunteerActivism";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
-import ErrorIcon from '@mui/icons-material/Error'
+import ErrorIcon from "@mui/icons-material/Error";
 import TextFields from "../../components/TextFields";
 import { userName, pawdRegExp } from "../../utils";
 import { register } from "../../sevives/useService";
@@ -36,19 +34,15 @@ function Copyright(props) {
   );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme();
-
 const schema = yup.object({
   username: yup
     .string()
     .required("Không được để trống!")
-    .matches(userName, "Sai định dạng tài khooản"),
+    .min(3, "Tên đăng nhập phải có ít nhất 3 ký tự"),
   password: yup
     .string()
     .required("Không được để trống!")
-    .matches(pawdRegExp, "Mật khẩu phải có ít nhất từ 6 tới 12 ký tự"),
+    .min(6, "Mật khẩu phải có ít nhất 6 ký tự"),
 });
 
 export default function Register(props) {
@@ -87,85 +81,73 @@ export default function Register(props) {
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <VolunteerActivismIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Đăng ký
+        </Typography>
         <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
+          component="form"
+          noValidate
+          onSubmit={handleSubmit(onSubmit)}
+          sx={{ mt: 3 }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <VolunteerActivismIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Đăng ký
-          </Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit(onSubmit)}
-            sx={{ mt: 3 }}
+          <TextFields
+            errors={errors}
+            control={control}
+            name="username"
+            label="Tên đăng nhập"
+          />
+          {message ? (
+            <>
+              <Typography color="error.main" variant="span" fontSize="14px">
+                {message}
+              </Typography>
+            </>
+          ) : (
+            ""
+          )}
+          <TextFields
+            errors={errors}
+            control={control}
+            name="password"
+            label="Mật khẩu"
+            type="password"
+          />
+
+          <Button
+            fullWidth
+            type="submit"
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
           >
-            <TextFields
-              errors={errors}
-              control={control}
-              name="username"
-              label="Tên đăng nhập"
-            />
-
-            <TextFields
-              errors={errors}
-              control={control}
-              name="password"
-              label="Mật khẩu"
-              type="password"
-            />
-
-            {message ? (
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  gap: "5px",
-                  mt: "6px",
+            Đăng ký
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link
+                href="#"
+                variant="body2"
+                onClick={() => {
+                  props.setSignIn(false);
                 }}
               >
-                <ErrorIcon color="error" sx={{ width: "20px" }} />
-                <Typography color="error.main" variant="span" fontSize="14px">
-                  {message}
-                </Typography>
-              </Box>
-            ) : (
-              ""
-            )}
-            <Button
-              fullWidth
-              type="submit"
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Đăng ký
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link
-                  href="#"
-                  variant="body2"
-                  onClick={() => {
-                    props.setSignIn(false);
-                  }}
-                >
-                  Bạn đã có tài khoản? Đăng nhập ngay.
-                </Link>
-              </Grid>
+                Bạn đã có tài khoản? Đăng nhập ngay.
+              </Link>
             </Grid>
-          </Box>
+          </Grid>
         </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
+      </Box>
+      <Copyright sx={{ mt: 5 }} />
+    </Container>
   );
 }
